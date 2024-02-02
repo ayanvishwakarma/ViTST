15547e50a0cfbb1717a6c8d4551b54c6432533ab
ayanvishwakarma <102685152+ayanvishwakarma@users.noreply.github.com>
2024-02-02 05:58:41 +0530

    Update load_data.py

--- code/Vision-Text/load_data.py
+++ code/Vision-Text/load_data.py
@@ -66,7 +66,7 @@ def get_data_split(base_path, split_path, split_idx, dataset='P12', prefix='', u
     elif task == "regression":
         y = y.astype(np.float32)
 
-    idx_train, idx_val, idx_test = np.load('/kaggle/input/vitst-data/'+ split_path, allow_pickle=True)
+    idx_train, idx_val, idx_test = np.load('/kaggle/input/vitst-data/phy12_split1.npy', allow_pickle=True)
     # extract train/val/test examples
     Ptrain = Pdict_list[idx_train]
     Pval = Pdict_list[idx_val]
